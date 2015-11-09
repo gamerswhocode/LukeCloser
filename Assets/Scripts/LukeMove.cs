28ded0dd14095080ba0da7fd303b9bb12422a0d9
alejandrocastrolopez <acalo91@gmail.com>
2015-11-08 23:31:11 -0800

    Added Luke Animations and Office Scene

--- Assets/Scripts/LukeMove.cs
+++ Assets/Scripts/LukeMove.cs
@@ -5,9 +5,12 @@ public class LukeMove : MonoBehaviour {
 	private RaycastHit hit;
 	private Ray ray;	
 	private Camera mainCamera;
+	private NavMeshAgent agent;
+	public bool moving = false;
 	// Use this for initialization
 	void Start () {	
 		mainCamera = GameObject.FindGameObjectWithTag ("MainCamera").camera;
+		agent = GetComponent<NavMeshAgent> ();
 	}
 	
 	// Update is called once per frame
@@ -19,8 +22,23 @@ public class LukeMove : MonoBehaviour {
 			if (Physics.Raycast(ray, out hit)) {
 				Vector3 objectHit = hit.point;
 				Debug.Log("Will move i guess");
-				GetComponent<NavMeshAgent>().SetDestination(objectHit);
+				agent.SetDestination(objectHit);
+			}
+		}
 
+		if (agent.velocity.magnitude > 0) 
+		{
+			if(!moving){
+				GetComponent<Animator> ().SetBool ("Moving", true);
+				moving = true;
+			}
+		} 
+		else
+		{
+			if(moving)
+			{
+				GetComponent<Animator> ().SetBool ("Moving", false);
+				moving = false;
 			}
 		}
 	
